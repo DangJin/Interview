81570c124cc56739ff0aed9ed3528dbdbe7771b9
dangjin <dangjin798@yeah.net>
2017-01-25 11:30:31 +0800

    main
    Signed-off-by: dangjin <dangjin798@yeah.net>

--- t_1/main.cpp
+++ t_1/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int w, h;
+    cin >> w >> h;
+    int pic_arr[w][h];
+    for (int i = 0; i < w; ++i) {
+        for (int j = 0; j < h; ++j) {
+            cin >> pic_arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < w; ++i) {
+        for (int j = 0; j < h; ++j) {
+            cout << pic_arr[i][j];
+        }
+        cout << "\n";
+    }
     return 0;
 }
