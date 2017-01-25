528431c98314ba1e39037cd532fd45217d536853
dangjin <dangjin798@yeah.net>
2017-01-25 17:06:08 +0800

    main
    Signed-off-by: dangjin <dangjin798@yeah.net>

--- t_1/main.cpp
+++ t_1/main.cpp
@@ -1,21 +1,50 @@
 #include <iostream>
 
 using namespace std;
-int main() {
-    int w, h;
-    cin >> w >> h;
-    int pic_arr[w][h];
-    for (int i = 0; i < w; ++i) {
-        for (int j = 0; j < h; ++j) {
-            cin >> pic_arr[i][j];
-        }
-    }
 
-    for (int i = 0; i < w; ++i) {
-        for (int j = 0; j < h; ++j) {
-            cout << pic_arr[i][j];
+int main() {
+    int n;
+    cin >> n;
+    for (int k = 0; k < n; ++k) {
+        int w, h;
+        cin >> w >> h;
+        //验证输入数据的合法性
+        int pic_arr[w][h];
+        for (int i = 0; i < w; ++i) {
+            for (int j = 0; j < h; ++j) {
+                cin >> pic_arr[i][j];
+            }
         }
-        cout << "\n";
+
+        //正向置零
+        for (int i = 0; i < w; ++i) {
+            for (int j = 0; j < h; ++j) {
+                if (pic_arr[i][j] != 0) {       /// 置零
+                    pic_arr[i][j] = 0;
+                } else if (pic_arr[i][j] == 0) {    /// 停止置零
+                    break;
+                }
+            }
+        }
+
+        //逆向置零
+        for (int i = w - 1; i >= 0; i--) {
+            for (int j = h - 1; j >= 0; j--) {
+                if (pic_arr[i][j] != 0) {       /// 置零
+                    pic_arr[i][j] = 0;
+                } else if (pic_arr[i][j] == 0) {    /// 停止置零
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < w; ++i) {
+            for (int j = 0; j < h; ++j) {
+                cout << pic_arr[i][j];
+            }
+
+            cout << "\n";
+        }
     }
     return 0;
 }
